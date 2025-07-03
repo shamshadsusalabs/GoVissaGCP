63b3707627a4a39f90cb03ab3e6375deec211960
shamshadsusalabs <Shamshad@susalabs.com>
2025-07-03 08:58:11 +0530

    djbjd

--- EasyOcr/main.py
+++ EasyOcr/main.py
@@ -199,5 +199,5 @@ async def extract_passport(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
